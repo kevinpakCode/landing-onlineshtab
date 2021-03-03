55b666fcf3b71d46a75daed3aca0276c0facc94e
Kevinpak <kevinpak@yandex.ru>
2021-03-03 14:25:31 +0300

    [FRN-111] add page 404

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import Styles from './../styles/pages/404.module.scss'
+
+const ErrorPage = () => {
+  return (
+    <>
+    <div className={Styles.ErrorPage}>
+      404
+    </div>
+    </>
+  )
+}
+
+export default ErrorPage
